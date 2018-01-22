3ec2740cdd5a3cd33f870063cf11f747199198e3
martincz <martincz@mokeedev.com>
2018-01-22 19:31:59 +0800

    sepolicy: mokee-ify
    Change-Id: Ic128880959e817eaecbfbf236375c9bfd6d8afad

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,8 +1,8 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from MoKee
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/mokee/sepolicy/qcom/common \
+    device/mokee/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
